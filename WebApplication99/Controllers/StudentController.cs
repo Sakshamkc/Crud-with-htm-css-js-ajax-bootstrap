d649ae1ff284f6a627282cde6705edf7eeaccf2b
Sakshamkc <35841224+Sakshamkc@users.noreply.github.com>
2022-03-15 12:16:31 +0545

    first commit

---
+++ WebApplication99/Controllers/StudentController.cs
@@ -0,0 +1,63 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication99.Models;
+using static WebApplication99.Helper;
+
+namespace WebApplication99.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentDbContext _context;
+
+        public StudentController(StudentDbContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+          var query = from x in _context.Students select x;
+          return View(query.ToList());
+        }
+        [HttpGet]
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new Student());
+            }
+            else
+            {
+              return View(_context.Students.Where(x => x.StudentId == id).FirstOrDefault<Student>());
+                
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(Student std)
+        {
+              if (std.StudentId == 0)
+                {
+                    _context.Students.Add(std);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    _context.Students.Update(std);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+            }
+            
+        }
+
+
+       
+     
+    }
+}
---
+++ WebApplication99/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication99.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required(ErrorMessage ="Name Field is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Name Field is required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Name Field is required")]
+        public string Email { get; set; }
+    }
+}
